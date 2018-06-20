f4020baf7bfbe7cba76936ec3750f53ab5f12775
Chih Cheng Liang <chihchengliang@gmail.com>
2018-06-20 23:19:22 +0800

    simple test_deposit

---
+++ validator_registration.v.py
@@ -0,0 +1,24 @@
+Deposit: event({
+    pubkey: bytes32,
+    withdrawal_shard_id: uint256,
+    withdrawal_addr: address,
+    randao_commitment: bytes32})
+
+
+used_pubkey: public(bool[bytes32])
+
+
+@public
+@payable
+def deposit(
+        pubkey: bytes32,
+        withdrawal_shard_id: uint256,
+        withdrawal_addr: address,
+        randao_commitment: bytes32):
+    assert msg.value == as_wei_value(32, "ether")
+    assert not self.used_pubkey[pubkey]
+
+    self.used_pubkey[pubkey] = True
+
+    log.Deposit(pubkey, withdrawal_shard_id,
+                withdrawal_addr, randao_commitment)
